29e26ecc12753f441dfe6063e165b03364be0018
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2022-02-13 20:50:47 -0300

    feat: Application created

---
+++ demo/src/main/kotlin/com/github/gabrielpadilh4/demo/controller/HelloWorld.kt
@@ -0,0 +1,15 @@
+package com.github.gabrielpadilh4.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HelloWorld {
+
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello World"
+    }
+}
